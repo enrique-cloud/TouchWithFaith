b07d3b5d277843d89014c5f0bc71bc01b4f98423
enrique <85899574+enrique-cloud@users.noreply.github.com>
2022-06-15 16:06:42 +0000

    url function

--- get_bible/data.py
+++ get_bible/data.py
@@ -6,17 +6,24 @@ import random
 books=["leviticus","hebrews","mark","luke","matthew","john","revelation","romans","numbers"]    # add more books
 version='valera' # asv, valera, there are more versions (lenguages)
 
+
+def url_build(books_,version_):
+  book_ = random.choice(books_)
+  url_ = f"https://getbible.net/json?passage={book_}&raw=true&version={version_}"
+  return url_, book_
+
+
 def bible(request):
-  book = random.choice(books)
-  url_hebrews = f"https://getbible.net/json?passage={book}&raw=true&version={version}"
+  url = url_build(books, version)[0]
+  book = url_build(books, version)[1]
   
-  list_chapters = list(range(1,len(requests.get(url_hebrews).json()['book'])+1))
+  list_chapters = list(range(1,len(requests.get(url).json()['book'])+1))
   choice_chapter = random.choice(list_chapters)
   
-  list_vers = list(range(1,len(requests.get(url_hebrews).json()['book'][f'{choice_chapter}']['chapter'])+1))
+  list_vers = list(range(1,len(requests.get(url).json()['book'][f'{choice_chapter}']['chapter'])+1))
   choice_vers = random.choice(list_vers)
   
-  chosen_text = requests.get(url_hebrews).json()['book'][f'{choice_chapter}']['chapter'][f'{choice_vers}']['verse']
+  chosen_text = requests.get(url).json()['book'][f'{choice_chapter}']['chapter'][f'{choice_vers}']['verse']
 
   phrase_description = {'phrase':chosen_text, 'book':book.upper(), 'chapter':choice_chapter, 'vers':choice_vers}
   
